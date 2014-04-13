5b247c56fce4596e0678ed85e5199545866f8f57
kingzyt <kingzyt2012@gmail.com>
2014-04-13 12:18:55 +0800

    ff

--- TestGame/Script/CsScript/MainClass.cs
+++ TestGame/Script/CsScript/MainClass.cs
@@ -82,7 +82,7 @@ namespace Game.Script
              *            }
              *           ...
              *     }
-             */
+             */ 
             var cacheSet = new ShareCacheStruct<Notice>();
             for (int i = 0; i < 50; i++)
             {
